0e0b057957524888baa41cb22ea7ce4ae6b19e4c
isaacpeh <isaacpeh123@gmail.com>
2022-07-20 18:11:02 +0800

    Initial commit for WS

--- assignment2_jad_client/src/main/java/controller/DatabaseConfig.java
+++ assignment2_jad_client/src/main/java/controller/DatabaseConfig.java
@@ -17,9 +17,10 @@ public class DatabaseConfig {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			//String connURL = "jdbc:mysql://ipecsql.ddns.net:3306/assignment2_jad_client?user=jadassignment1&password=P@ssw0rd&serverTimezone=UTC";
-			// String connURL = "jdbc:mysql://192.168.1.30:3306/assignment2_jad_client?user=jadassignment1&password=P@ssw0rd&serverTimezone=UTC";
-			String connURL = "jbdc:mysql://localhost:3306/assignment2_jad_client?user=root&password=Diuleil0m097&serverTimezone=UTC";
+			// String connURL =
+			// "jdbc:mysql://ipecsql.ddns.net:3306/assignment2_jad_client?user=jadassignment&password=P@ssw0rd&serverTimezone=UTC";
+			String connURL = "jdbc:mysql://192.168.1.30:3306/assignment2_jad_client?user=jadassignment&password=P@ssw0rd&serverTimezone=UTC";
+			// String connURL = "jbdc:mysql://localhost:3306/assignment2_jad_client?user=root&password=Diuleil0m097&serverTimezone=UTC";
 			conn = DriverManager.getConnection(connURL);
 
 		} catch (Exception ex) {
