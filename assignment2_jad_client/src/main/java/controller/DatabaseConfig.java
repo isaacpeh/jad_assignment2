0437ddfbf6113d81cfd20bb9aa272c0394f1737b
isaacpeh <isaacpeh123@gmail.com>
2022-07-28 15:46:14 +0800

    Update dbconfig.js

--- assignment2_jad_client/src/main/java/controller/DatabaseConfig.java
+++ assignment2_jad_client/src/main/java/controller/DatabaseConfig.java
@@ -17,7 +17,7 @@ public class DatabaseConfig {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			//String connURL = "jdbc:mysql://ipecsql.ddns.net:3306/assignment2_jad_client?user=jadassignment1&password=P@ssw0rd&serverTimezone=UTC";
+			//String connURL = "jdbc:mysql://ipecsql.ddns.net:3306/assignment2_jad_client?user=jadassignment&password=P@ssw0rd&serverTimezone=UTC";
 			String connURL = "jdbc:mysql://localhost:3306/assignment2_jad_client?user=root&password=<yourownpassword>&serverTimezone=UTC";
 			conn = DriverManager.getConnection(connURL);
 
