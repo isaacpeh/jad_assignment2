a8103741ab1d728a31e280014e53210d0e5f11f8
George Yin Xu <georgeyinxu@gmail.com>
2022-08-03 22:20:15 +0800

    Fixed some merge conflicts

--- assignment2_jad_client/src/main/java/controller/DatabaseConfig.java
+++ assignment2_jad_client/src/main/java/controller/DatabaseConfig.java
@@ -18,11 +18,7 @@ public class DatabaseConfig {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String connURL = "jdbc:mysql://ipecsql.ddns.net:3306/assignment2_jad?user=jadassignment&password=P@ssw0rd&serverTimezone=UTC";
-<<<<<<< HEAD
 //			String connURL = "jdbc:mysql://localhost:3306/assignment2_jad?user=root&password=<yourownpassword>&serverTimezone=UTC";
-=======
-			// String connURL = "jdbc:mysql://localhost:3306/assignment2_jad?user=root&password=<yourownpassword>&serverTimezone=UTC";
->>>>>>> main
 			conn = DriverManager.getConnection(connURL);
 
 		} catch (Exception ex) {
